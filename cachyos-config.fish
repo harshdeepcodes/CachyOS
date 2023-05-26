f7c59a2f1f0f3a1676debc918bfb400ede69d161
Harshdeep Singh <94488766+harshluckyme@users.noreply.github.com>
2023-05-26 13:46:20 +0530

    Create cachyos-config.fish

---
+++ cachyos-config.fish
@@ -0,0 +1,8 @@
+# Directory: /usr/share/cachyos-fish-config/
+# CUSTOM ALIASES
+
+alias webssh="ssh -p 22 harsh@192.168.122.78"
+alias update="sudo pacman -Syyu"
+alias pacin="sudo pacman -S $1"
+alias pipin="pip install $1"
+alias pip3in="pip3 install $1"
